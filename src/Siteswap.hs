2ccc97caed33eaafc15d28b6c7acb53b63df196b
Patrick Herrmann <patrickwherrmann@gmail.com>
2015-04-11 18:35:35 -0700

    Reverse state strings

--- src/Siteswap.hs
+++ src/Siteswap.hs
@@ -29,7 +29,7 @@ propCount :: SiteswapState -> PropCount
 propCount (SiteswapState s) = PropCount $ popCount s
 
 showState :: MaxThrow -> SiteswapState -> String
-showState (MaxThrow mt) (SiteswapState s) = bin <$> [mt, mt - 1..0]
+showState (MaxThrow mt) (SiteswapState s) = bin <$> [0..mt]
   where bin i
           | testBit s i = '1'
           | otherwise = '0'
